3e8b48ba492f23fee8c90691f797043cf5bf8bb2
pttxxxhit <ernestomerinocc@gmail.com>
2025-09-21 02:49:05 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,10 +2,7 @@ const express = require('express');
 const app = express();
 const helmet = require('helmet');
 app.use(helmet());
-app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({ action: 'deny' }));
-
-
 middleware helmet.hidePoweredBy()
 
 
