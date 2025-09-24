404f7d96ccc334eb8b7337e99f7f798b3d4ee1aa
pttxxxhit <ernestomerinocc@gmail.com>
2025-09-23 23:19:42 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -4,6 +4,8 @@ const app = express();
 app.use(helmet.hidePoweredBy());
 app.use(helmet.xssFilter({}));
 app.use(helmet.noSniff())));
+ninetyDaysInSeconds = 90*24*60*60
+app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
 app.use(helmet.ieNoOpen());
 app.use(helmet());
 app.use(helmet.frameguard({ action: 'deny' }));
